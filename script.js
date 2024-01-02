18103f14d26783ecea6bd29dbc3dc286205d34ca
ChadM95 <130190686+ChadM95@users.noreply.github.com>
2024-01-02 12:15:13 +0000

    add jquery + add show/hide to index.html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//JavaScript
+
 function func1() {
     let name = prompt("What is your name?");
     alert("Hello, " + name);
@@ -11,3 +13,15 @@ function funcShow() {
 function funcHide() {
     document.getElementById('lightBulb').style.visibility='hidden';
 }
+
+//Jquery
+
+$(document).ready(function() {
+    $('#show').click(function(){
+        $('#about').show();
+    $('#hide').click(function(){
+        $('#about').hide();
+    })
+    
+})
+})
